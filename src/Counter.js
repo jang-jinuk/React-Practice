a338709263b66e8f787197e57bc42c9911a357b7
jang-jinuk <dlsndifn98@gmail.com>
2024-10-12 22:27:47 +0900

    this.setState에 객체 대신 함수 인자 전달

--- src/Counter.js
+++ src/Counter.js
@@ -13,7 +13,16 @@ class Counter extends Component {
         <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            //위 코드와 아래 코드는 완전히 똑같은 기능을 함
+            //아래 코드는 함수에서 바로 객체를 반환
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
           }}
         >
           +1
